bb49190bc7106851eb88ab93c382efe1865ffb60
Ziva Lee <zivalee87@gmail.com>
2018-05-17 16:43:45 +0800

    Add water and idea pages

--- app/src/main/java/com/example/ziva/myapplication/add_water.java
+++ app/src/main/java/com/example/ziva/myapplication/add_water.java
@@ -4,9 +4,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
 
 public class add_water extends AppCompatActivity {
 
+    private EditText edDate2;
+    private EditText edInfo2;
+    private EditText edAmount2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,12 @@ public class add_water extends AppCompatActivity {
         // 返回鍵
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+    private void findViews() {
+        edDate2 = (EditText) findViewById(R.id.ed_date2);
+        edInfo2 = (EditText) findViewById(R.id.ed_info2);
+        edAmount2 = (EditText) findViewById(R.id.ed_amount2);
     }
 }
+
